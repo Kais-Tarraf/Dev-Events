0d29a4ee1777623627a5a04641ee397ff70cc21e
Kais Tarraf <hossamtr184@gmail.com>
2026-09-26 12:40:04 +0300

    add similar events, implement booking

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
 import { IEvent } from "@/database/event.model";
+import { cacheLife } from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const page = async () => {
+	"use cache";
+	cacheLife("hours");
 	const response = await fetch(`${BASE_URL}/api/events`);
 	const { events } = await response.json();
 	return (
@@ -24,7 +27,7 @@ const page = async () => {
 					{events &&
 						events.length > 0 &&
 						events.map((event: IEvent) => (
-							<li key={event.title}>
+							<li key={event.title} className="list-none">
 								<EventCard {...event} />
 							</li>
 						))}
